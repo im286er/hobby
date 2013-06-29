b7f961371326f3dc760daada6beee26cb9d3014d
usual2970 <536464346@qq.com>
2013-06-29 08:43:49 +0800

    Update admin.ctrl.php

--- app/control/admin.ctrl.php
+++ app/control/admin.ctrl.php
@@ -3,6 +3,7 @@ class AdminControl extends Control{
 
 	function index(){
 		$site_url=site_url();
+		echo "1341";
 		load_view("admin_head",array("site_url"=>$site_url));
 		load_view("admin_main",array("site_url"=>$site_url));
 		load_view("admin_foot",array("site_url"=>$site_url));
